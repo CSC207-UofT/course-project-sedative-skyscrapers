86d7a055fdc44d376a86cc8c4320093686193dc8
khushaal-nandwani <79563466+khushaal-nandwani@users.noreply.github.com>
2021-11-15 01:20:03 -0500

    Update GetTaskDetails.java

--- phase1/src/main/java/database/GetTaskDetails.java
+++ phase1/src/main/java/database/GetTaskDetails.java
@@ -5,61 +5,61 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class GetTaskDetails {
-	DataMiner data;
+    DataMiner data;
 
-	{
-		try {
-			data = new DataMiner();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    {
+        try {
+            data = new DataMiner();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public GetTaskDetails() {
+    public GetTaskDetails() throws FileNotFoundException {
 
-	}
+    }
 
-	public ArrayList<String> getTaskDetails(String taskID) {
-		ArrayList<String> taskDetails = new ArrayList<String>();
+    public ArrayList<String> getTaskDetails(String taskID) throws FileNotFoundException{
+        ArrayList<String> taskDetails = new ArrayList<String>();
 
-		String[] attributes = new String[0];
-		try {
-			attributes = data.get_line("raffleTaskDetails", true);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		while(true) {
-			if (attributes[1].equals(taskID)) {
-				taskDetails.add(attributes[2]);
-				taskDetails.add(attributes[3]);
-				taskDetails.add(attributes[4]);
-				break;
-			}
-			try {
-				attributes = data.get_line("raffleTaskDetails", false);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+        String[] attributes = new String[0];
+        try {
+            attributes = data.get_line("raffleTaskDetails", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while(true) {
+            if (attributes[1].equals(taskID)) {
+                taskDetails.add(attributes[2]);
+                taskDetails.add(attributes[3]);
+                taskDetails.add(attributes[4]);
+                break;
+            }
+            try {
+                attributes = data.get_line("raffleTaskDetails", false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-		return taskDetails;
-	}
+        return taskDetails;
+    }
 
-	public ArrayList<String> getUsedTaskIDs() {
-		ArrayList<String> usedTaskIDs = new ArrayList<String>();
+    public ArrayList<String> getUsedTaskIDs() throws IOException {
+        ArrayList<String> usedTaskIDs = new ArrayList<String>();
 
-		String[] attributes = new String[0];
-		try {
-			attributes = data.get_line("raffleTaskDetails", true);
-			attributes = data.get_line("raffleTaskDetails", false);
-			while (attributes != null) {
-				usedTaskIDs.add(attributes[1]);
-				attributes = data.get_line("raffleTaskDetails", false);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        String[] attributes = new String[0];
+        try {
+            attributes = data.get_line("raffleTaskDetails", true);
+            attributes = data.get_line("raffleTaskDetails", false);
+            while (attributes != null) {
+                usedTaskIDs.add(attributes[1]);
+                attributes = data.get_line("raffleTaskDetails", false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		return usedTaskIDs;
-	}
+        return usedTaskIDs;
+    }
 }
