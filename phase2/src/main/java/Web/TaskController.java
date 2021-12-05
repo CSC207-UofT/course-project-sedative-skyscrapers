d0a6ebbfee737a77f362ce63622f06f2282f46fe
Garv Mohan Sood <garvmohansood@yahoo.com>
2021-12-06 05:09:43 +0530

    Lookup, Creation and Executor Task controllers merged into a single controller class. Director class to build controller objects with respective attributes.

---
+++ phase2/src/main/java/Web/TaskController.java
@@ -0,0 +1,102 @@
+package main.java.Web;
+
+import main.java.TaskComponent.CreateTaskUseCase;
+import main.java.TaskComponent.ExecuteCommandUseCase;
+import main.java.TaskComponent.TaskLookupUseCase;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TaskController{
+    private String raffleID;
+    private String taskName;
+    private String description;
+    private String taskLink;
+    private String taskID;
+    private ArrayList<String> taskInfo;
+
+    public enum taskAction{
+        CREATE, EXECUTE, LOOKUP
+    }
+
+    public TaskController(){
+        // they are all set to null for the Task Director to initialize
+        this.taskID = null;
+        this.taskLink = null;
+        this.taskName = null;
+        this.description = null;
+        this.raffleID = null;
+        this.taskInfo = null;
+    }
+
+    public void runTaskController(taskAction actionToProcess) throws Exception {
+        switch(actionToProcess){
+            case CREATE:
+                if((taskName != null) && (taskLink != null) && (description != null)){
+                    // a task can be created only if tas name, link and description is given
+                    this.runCreate();
+                }
+
+
+            case EXECUTE:
+                if ((raffleID != null) && (taskID != null)){
+                    // task is executed when raffleID and taskID are given
+                    this.runExecute();
+                }
+
+            case LOOKUP:
+                if((taskID != null) && (raffleID == null)){
+                    // when only taskID is provided
+                    this.runLookup();
+                }
+
+        }
+    }
+
+public void runCreate() throws IOException {
+        CreateTaskUseCase createTaskManager = new CreateTaskUseCase(this.raffleID, this.taskName, this.description, this.taskLink);
+        this.taskID = createTaskManager.runTaskCreation();
+}
+
+public void runExecute() throws Exception {
+        ExecuteCommandUseCase executeManager = new ExecuteCommandUseCase(this.raffleID, this.taskID);
+        executeManager.RunExecuteCommand();
+}
+
+public void runLookup() throws FileNotFoundException {
+        TaskLookupUseCase lookupManager = new TaskLookupUseCase(this.taskID);
+        this.taskInfo = lookupManager.getTaskInfo();
+}
+
+// setters
+
+public void setRaffleID(String raffleID){
+        this.raffleID = raffleID;
+}
+
+public void setTaskID(String taskID){
+        this.taskID = taskID;
+}
+
+public void setDescription(String description){
+        this.description = description;
+}
+
+public void setTaskLink(String taskLink){
+        this.taskLink = taskLink;
+}
+
+public void setTaskName(String taskName){
+        this.taskName = taskName;
+}
+// getters
+
+public String getTaskID(){
+        return this.taskID;
+}
+
+public ArrayList<String> getTaskInfo(){
+        return this.taskInfo;
+}
+}
---
+++ phase2/src/main/java/Web/TaskDirector.java
@@ -0,0 +1,42 @@
+
+package main.java.Web;
+
+
+/**
+ * This class builds the TaskController objects with their respective attributes.
+ */
+
+public class TaskDirector{
+    private TaskController TaskContr;
+
+    public TaskDirector(){
+        this.reset();
+    }
+
+    private void reset(){
+        this.TaskContr = new TaskController();
+    }
+
+    public void taskBuildCreator(String raffleID, String taskName, String description, String taskLink){
+        // sets the values of all required variables in order to instantiate a task
+        this.TaskContr.setRaffleID(raffleID);
+        this.TaskContr.setDescription(description);
+        this.TaskContr.setTaskLink(taskLink);
+        this.TaskContr.setTaskName(taskName);
+    }
+
+    public void taskBuildExecutor(String raffleID, String taskID){
+        // sets values of all variables required to execute a task (completing a task)
+        this.TaskContr.setRaffleID(raffleID);
+        this.TaskContr.setTaskID(taskID);
+    }
+
+    public void taskBuildLookup(String taskID){
+        // sets values of variables required to lookup tasks using taskID
+        this.TaskContr.setTaskID(taskID);
+    }
+
+    public TaskController getTaskContr() {
+        return TaskContr;
+    }
+}
