55c35d4eb8f3905ea8d35197292e820d2537671e
khushaal-nandwani <79563466+khushaal-nandwani@users.noreply.github.com>
2021-12-05 23:45:13 -0500

    Update DataAccessPoint.java

--- phase2/src/main/java/RaffleComponent/DataAccessPoint.java
+++ phase2/src/main/java/RaffleComponent/DataAccessPoint.java
@@ -9,57 +9,36 @@ public interface DataAccessPoint {
      by the use cases (directly dependent on this interface) to get the needed information from the database
      */
 
-    ArrayList<String> getTakenRaffleIds() throws SQLException;
-    // NOTED
-    // obtain data from DB about ids that are already under use (Already implemented)
+     ArrayList<String> getTakenRaffleIds() throws SQLException;
 
-    ArrayList<Object> getOrganizerRaffleById(String orgRaffleId) throws SQLException, ParseException;
-    // NOTED
-    // get the information specific to a raffle according to its id (indexing of the hashmap
-    // from getAllOrganizerRaffles)
-    // [raffleName="raffle", numberOfWinners=2, rules="this is a string of rules",
-    //        endDate=LocalDate.of(2021, 12, 25), taskIds=ArrayList<String>, ptcIds=ArrayList<String>,
-    //        winnerIds=ArrayList<String>]
+     ArrayList<Object> getOrganizerRaffleById(String orgRaffleId) throws SQLException, ParseException;
 
-    ArrayList<Object> getParticipantRaffleById(String ptcRaffleID) throws SQLException, ParseException;
-    // trim raffleID
-    // Hence, NOTED
-    // Returns the list of participants in that raffle  [raffleName, numOfWinners, rules, endDate, taskIdList]
+     ArrayList<Object> getParticipantRaffleById(String ptcRaffleID) throws SQLException, ParseException;
 
-    ArrayList<String> getTaskById(String taskId);
-    // NOTED
+     ArrayList<String> getTaskById(String taskId);
 
-    ArrayList<String> getParticipantInfo(String ptcUsername);
-    // NOTED
-    // Return the information about the participant
 
-    ArrayList<String> getOrganizerInfo(String orgUsername);
-    // NOTED
+     ArrayList<String> getParticipantInfo(String ptcUsername);
+     
+     ArrayList<String> getOrganizerInfo(String orgUsername);
 
-    ArrayList<String> getParticipantRaffleIds(String ptcUsername);
-    // NOTED AS: returns the RaffleIds in which Participant is enrolled in
-    // "participantusername:raffleID"
+     ArrayList<String> getParticipantRaffleIds(String ptcUsername);
 
-    ArrayList<String> getOrganizerRaffleIds(String orgUsername);
-    // NOTED AS: returns the raffleIDs of raffles which are created by the Organizer with orgusername
-    // todo these are the new ones from nov 23, the rest still need to be implemented though
-    // "raffleID"
+     ArrayList<String> getOrganizerRaffleIds(String orgUsername);
 
-    boolean hasCompletedTask(String ptcRaffleId, String userName, String taskId) throws SQLException;
-    // NOTED AS: <userName> has to be a <ptcusername>. Returns whether the ptc has completed the task in the raffle
-    // return whether the specified task has the "complete" status
+     boolean hasCompletedTask(String ptcRaffleId, String userName, String taskId) throws SQLException;
 
-    ArrayList<String> getTakenOrganizerIds();
-//     output: all organizer IDs stored
+     ArrayList<String> getTakenOrganizerIds();
 
-    ArrayList<String> getTakenParticipantIds();
-//     output: all participant IDs stored
+     ArrayList<String> getTakenParticipantIds();
 
-    ArrayList<String> getValidParticipants (String orgRaffleId);
-    // returns arrayList of the ids of all participants who have completed all of the tasks of this specific raffle,
-    // I could pass you the raffle's taskIds too if you need them. This method was implemented for phase1 already,
-    // I just oversaw it when looking at the interface, but it shouldnt be too bad using phase1's implementation as
-    // a guide (inside dataExtractor)
-//
-}
+
+     ArrayList<String> getValidParticipants (String orgRaffleId);
+
+     ArrayList<String> getRaffleIDByRaffleName(String raffleName);
+
+     ArrayList<String> getOrgIDByOrganization(String organization);
+
+     ArrayList<String>  getTakenTaskIds();
+ }
 
